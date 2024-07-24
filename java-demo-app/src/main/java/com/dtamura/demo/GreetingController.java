21764836fa6acd4430358b98d66bd6d8256280e2
Daiki Tamura <tamura.daiki.dc3@jp.nssol.nipponsteel.com>
2024-07-24 03:59:31 +0000

    実装

--- java-demo-app/src/main/java/com/dtamura/demo/GreetingController.java
+++ java-demo-app/src/main/java/com/dtamura/demo/GreetingController.java
@@ -8,6 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // TODO (Trace) import文を追加ここから
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
+import java.util.ArrayList;
+import java.util.List;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.propagation.TextMapGetter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 // ここまで
 
@@ -20,33 +30,74 @@ public class GreetingController {
     private final Logger logger = LogManager.getLogger(GreetingController.class.getName());
 
     // TODO (Trace) Tracerプライベート変数を追加
+    private final Tracer tracer;
+    private final OpenTelemetry openTelemetry;
+
+    // HTTPヘッダから、トレースID等の情報を抽出するために使われるgetter関数
+    private final TextMapGetter<HttpHeaders> getter = new TextMapGetter<HttpHeaders>() {
+        @Override
+        public String get(HttpHeaders headers, String s) {
+            assert headers != null;
+            List<String> list = headers.get(s);
+            if (list != null) {
+                return list.get(0);
+            }
+            return null;
+        }
+
+        @Override
+        public Iterable<String> keys(HttpHeaders headers) {
+            List<String> keys = new ArrayList<>();
+            headers.forEach((k, v) -> {
+                keys.add(k);
+            });
+            return keys;
+        }
+    };
 
     // TODO (Trace) コンストラクタの引数にOpenTelemetryを追加
     // TODO (Trace) コンストラクタ内でtracer変数を初期化
-    public GreetingController() {
+    public GreetingController(OpenTelemetry openTelemetry) {
+        this.openTelemetry = openTelemetry;
+        this.tracer = openTelemetry.getTracer(GreetingController.class.getName(), "0.1.0");
     }
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+            @RequestHeader HttpHeaders headers) {
 
-        // TODO (Trace) スパンの開始コードを追加
+        // TODO スパンの開始コードを追加
+        Context extractedContext = this.openTelemetry.getPropagators().getTextMapPropagator()
+                .extract(Context.current(), headers, this.getter);
+        Span span = tracer.spanBuilder("greeting").setParent(extractedContext).startSpan();
+
+        span.setAttribute("name", name);
+
 
         logger.info("start greeting");
 
-        hoge();
-        // TODO (Trace) スパンの終了コードを追加
+        try (Scope scope = span.makeCurrent()) {
+            hoge();
+        } finally {
+            // TODO スパンの終了コードを追加
+            span.setAttribute(io.opentelemetry.semconv.SemanticAttributes.HTTP_RESPONSE_STATUS_CODE, 200);
+            span.setAttribute(io.opentelemetry.semconv.SemanticAttributes.HTTP_REQUEST_METHOD, "GET");
+
+            span.end();
+        }
 
         logger.info("end greeting");
-
-        // TODO (Trace) GreetingオブジェクトにTraceIdを渡す
-        return new Greeting(counter.incrementAndGet(), String.format(template, name), "");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name),
+                span.getSpanContext().getTraceId());
     }
 
     public void hoge() {
         // TODO (Trace) スパンの開始コードを追加
+        Span span = tracer.spanBuilder("hoge").startSpan();
 
         logger.info("hoge");
         // TODO (Trace) スパンの終了コードを追加
+        span.end();
 
     }
 }
