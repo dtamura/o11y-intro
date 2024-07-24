83c54ae893bdb2e0c622e858fca699b36913b2b1
Daiki Tamura <tamura.daiki.dc3@jp.nssol.nipponsteel.com>
2024-07-24 05:40:31 +0000

    fix

--- java-demo-app/src/main/java/com/dtamura/demo/GreetingController.java
+++ java-demo-app/src/main/java/com/dtamura/demo/GreetingController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // TODO (Trace) import文を追加ここから
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+import io.opentelemetry.instrumentation.annotations.SpanAttribute;
 
 // ここまで
 
@@ -26,8 +28,9 @@ public class GreetingController {
     public GreetingController() {
     }
 
+    @WithSpan
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") @SpanAttribute("name") String name) {
 
         // TODO (Trace) スパンの開始コードを追加
 
@@ -42,6 +45,7 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(), String.format(template, name), "");
     }
 
+    @WithSpan
     public void hoge() {
         // TODO (Trace) スパンの開始コードを追加
 
